8f64376a315dfdb633a039e1ce0ba439f5b1941c
Khaled Alshaer <kalshaer@lab1r4s12.42abudhabi.ae>
2022-08-23 08:13:31 +0400

    first sub.

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: kalshaer <kalshaer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 08:30:28 by kalshaer          #+#    #+#             */
-/*   Updated: 2022/08/17 10:48:04 by kalshaer         ###   ########.fr       */
+/*   Updated: 2022/08/23 07:57:26 by kalshaer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*r;
+
 	if (size != 0 && count > SIZE_MAX / size)
 		return (0);
 	r = (void *) malloc(count * size);
